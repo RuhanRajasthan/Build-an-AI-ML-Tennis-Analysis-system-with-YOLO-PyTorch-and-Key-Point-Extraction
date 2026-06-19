5828cd47c993c987430656ddbf054a80a469ac81
holyb3anS <144182793+RuhanRajasthan@users.noreply.github.com>
2026-06-18 18:26:37 -0600

    idk man

---
+++ yolo_inference.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO('yolo26x')
+
+result = model.predict('input_video.mp4', save=True)
+print(result)
+print("boxes: ")
+for box in result[0].boxes:
+    print(box)
